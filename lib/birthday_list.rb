7f60715b97e8b19a6429a2c7c09f75f04ff262ca
Jeremyrrsmith <jeremyrrsmith@gmail.com>
2019-03-08 12:06:42 +0000

    Have remade the rspecs and code as needed the practice, currenly adding hash strings to an arry to hold the name and dob which works ok

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,24 +1,45 @@
 # Store all of my friends’ birthdays so I can keep track of them
 # See them all at once with their names and birthdays each on a line in a tidy format
 # Check whose birthday it is today - the program can check through the birthdays I have stored and check each one to see if it’s someone’s birthday, and tells me something like "It's Mary Poppin's birthday today! They are 124 years old!" - otherwise it won't say anything.
-require 'date'
+# require 'date'
+
 
 class BirthdayList
-  attr_reader :birthday_list_object
+  attr_reader :birthday_list_array
 
   def initialize
-    @birthday_list_object = []
+    @birthday_list_array = []
   end
 
-  def add(name='default name', dob=(Date.new(1900,1,1)))
-    @birthday_list_object << {name.to_s => dob}
+  def add(name, dob)
+    @birthday_list_array << {name => dob}
   end
 
-  def remove(name)
-    @birthday_list_object.pop[name]
-  end
 end
 
-example = BirthdayList.new
 
-p example.add('Jeremy')
+
+
+#   def add(name, dob)
+#     @birthday_list_object << {name => dob}]
+#   end
+
+#   def remove(name)
+#     @birthday_list_object.delete(name)
+#   end
+
+#   def print
+#     p "TESTING"
+
+#     @birthday_list_object.each do |this|
+    
+#       this.each do |key, value| 
+#         puts "> #{key}s birthday is #{value}"
+#       end
+#     end
+#   end
+
+# end
+
+
+
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -9,22 +9,67 @@ describe "BirthdayList" do
   end
 end
 
-  describe "#initialize" do
-    it "Creates an Array" do
-      # arrange + act
-      bday = BirthdayList.new
-      # assess
-      expect(bday.birthday_list_object).to be_kind_of(Array)
-    end
-    
-  describe "#add" do
-    it "Adds a name to the array as a hash key" do
-      # arrange + act
-      bday = BirthdayList.new
-      bday.add('Jeremy')
-      # assess
-      expect(bday.birthday_list_object).to include('Jeremy')
-    end
+describe "#initialize" do
+  it "Makes an array to enable us to add things" do
+    # arrange + act
+    example = BirthdayList.new
+    # assess
 
+    expect(example.birthday_list_array).to be_kind_of(Array)
   end
+end
+
+describe "#add" do
+  it "Can add a name/dob to the array as a hash" do
+    # arrange + act
+    example = BirthdayList.new
+    # assess
+    expect(example.add('Jeremy', '09/07/1982')).to eql([{'Jeremy' => '09/07/1982'}])
+  end
+
+  it "Can add multiple names/dobs to the array as a hash" do
+    # arrange + act
+    example_list = BirthdayList.new
+    example_list.add('bob', '01/01/1999')
+    array = example_list.add('Jeremy', '09/07/1982')
+    # assess
+    expect(array).to eql([{'bob' => '01/01/1999'}, {'Jeremy' => '09/07/1982'}])
+  end
+end
+
+
+
+
+  #   # describe "#print" do
+  #   #   it "Prints the birthday list" do
+  #   #     # arrange + act
+  #   #     example = BirthdayList.new
+  #   #     # assess
+  #   #     expect(example.print).to be_an_instance_of(example)
+  #   #   end
+  #   # end
+  #   # describe "#remove" do
+  #   #   it "Removes the selected element from the birthday list" do
+  #   #     # arrange + act
+  #   #     ........
+  #   #     # assess
+  #   #     expect().to be_kind_of()
+  #   #   end
   
+
+  # describe "#add" do
+  #   it "Adds a name to the array as a hash key" do
+  #     # arrange + act
+  #     example = BirthdayList.new
+  #     # assess
+  #     expect(example.add("Jeremy", '09/07/1982')).to eql([{"Jeremy" => '09/07/1982'}])
+  #   end
+  #   it "Adds a second name to the array as a hash key" do
+  #     # arrange + act
+  #     example = BirthdayList.new
+  #     example.add('bob', '1/1/2001')
+  #     result = example.add("Jeremy", '09/07/1982')
+  #     # assess
+  #     expect(result).to eql([{'bob'=>'1/1/2001'},{"Jeremy"=>"09/07/1982"])
+  #   end
+  # end
