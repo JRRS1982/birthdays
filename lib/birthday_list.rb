9e4c86d3b784ffe1cd4c4574af92ff7528f31212
Jeremyrrsmith <jeremyrrsmith@gmail.com>
2019-03-08 09:31:51 +0000

    Have done a few things such as add arguments to the add method set defaults etc work in progress"

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,1 +1,24 @@
+# Store all of my friends’ birthdays so I can keep track of them
+# See them all at once with their names and birthdays each on a line in a tidy format
+# Check whose birthday it is today - the program can check through the birthdays I have stored and check each one to see if it’s someone’s birthday, and tells me something like "It's Mary Poppin's birthday today! They are 124 years old!" - otherwise it won't say anything.
+require 'date'
 
+class BirthdayList
+  attr_reader :birthday_list_object
+
+  def initialize
+    @birthday_list_object = []
+  end
+
+  def add(name='default name', dob=(Date.new(1900,1,1)))
+    @birthday_list_object << {name.to_s => dob}
+  end
+
+  def remove(name)
+    @birthday_list_object.pop[name]
+  end
+end
+
+example = BirthdayList.new
+
+p example.add('Jeremy')
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -1,1 +1,30 @@
+require './lib/birthday_list.rb'
 
+describe "BirthdayList" do
+  it "Returns confirmation that a new object of Birthday is a Class" do
+    # arrange + act
+    example = BirthdayList.new
+    # assess
+    expect(BirthdayList.new.class).to be_kind_of(Class)
+  end
+end
+
+  describe "#initialize" do
+    it "Creates an Array" do
+      # arrange + act
+      bday = BirthdayList.new
+      # assess
+      expect(bday.birthday_list_object).to be_kind_of(Array)
+    end
+    
+  describe "#add" do
+    it "Adds a name to the array as a hash key" do
+      # arrange + act
+      bday = BirthdayList.new
+      bday.add('Jeremy')
+      # assess
+      expect(bday.birthday_list_object).to include('Jeremy')
+    end
+
+  end
+  
